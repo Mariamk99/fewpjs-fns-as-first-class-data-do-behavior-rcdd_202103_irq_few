283a58ac3857e1127e5ae2ae9d2a82050122f198
Mariam  Khamo <mariamw736@gmail.com>
2021-05-14 09:04:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,14 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
-function greet(){
-  
+function greet(timeStr){
+  let nowTime = timeStr.split(':');
+  let nowHour = parseInt(nowTime[0]);
+  if (nowHour<12){
+    return "Good Morning";
+  } else if (nowHour>17){
+    return "Good Evening";
+  }
+  return "Good Afternoon";
 }
 /* Write your implementation of displayMessage() */

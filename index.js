5976d4b7a8a875097db7e45b55cbeaa2ff0a51cf
Mariam  Khamo <mariamw736@gmail.com>
2021-05-14 09:05:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,3 +21,6 @@ function greet(timeStr){
   return "Good Afternoon";
 }
 /* Write your implementation of displayMessage() */
+function displayMessage(msg){
+  document.getElementById("greeting").innerText = msg;
+} 
